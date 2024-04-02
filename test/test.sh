6319af5f033ad158b4cc75ad10882eac576daee3
matiasuno <matias.vilches.a@usach.cl>
2024-04-02 18:27:08 -0300

    instances created

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 cd ../Cell2FireC
 pwd
 
 #RUN CANADIAN FBP INSTANCE
-./Cell2Fire.Linux.x86_64 --input-instance-folder ../data/CanadianFBP/400cellsC1 --output-instance-folder resutls/400cellsC1 --nsims 5 --out-messages --weather rows --sim C
+#./Cell2Fire.Linux.x86_64 --input-instance-folder ../data/test/400cellsC1 --output-instance-folder ../test/results/FBP --nsims 5 --out-messages --weather rows --sim C
 
 #RUN KITRAL INSTANCE
 #./Cell2Fire.Linux.x86_64 --input-instance-folder ../data/CanadianFBP/400cellsC1 --output-instance-folder resutls/400cellsC1 --nsims 5 --out-messages --weather rows --sim C
