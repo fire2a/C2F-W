79fa6dd3c80a7aab1fc772d4e7446b8c7a87538f
matiasuno <matias.vilches.a@usach.cl>
2024-04-02 23:53:16 -0300

    testing result added

--- test/test.sh
+++ test/test.sh
@@ -3,10 +3,10 @@ cd ../Cell2FireC
 pwd
 
 #RUN CANADIAN FBP INSTANCE
-#./Cell2Fire.Linux.x86_64 --input-instance-folder ../data/test/400cellsC1 --output-instance-folder ../test/results/FBP --nsims 5 --out-messages --weather rows --sim C
+./Cell2Fire.Linux.x86_64 --input-instance-folder ../test/FBP_Instance --output-folder ../test/test_results/FBP --sim-years 1 --nsims 100 --output-messages --out-ros --out-fl --out-intensity --grids --sim C
 
 #RUN KITRAL INSTANCE
-#./Cell2Fire.Linux.x86_64 --input-instance-folder ../data/CanadianFBP/400cellsC1 --output-instance-folder resutls/400cellsC1 --nsims 5 --out-messages --weather rows --sim C
+./Cell2Fire.Linux.x86_64 --input-instance-folder ../test/Kitral_Instance --output-folder ../test/test_results/Kitral --sim-years 1 --nsims 100 --output-messages --out-ros --out-fl --out-intensity --grids --sim K
 
 #RUN SCOTT AND BURGAN INSTANCE
-#./Cell2Fire.Linux.x86_64 --input-instance-folder ../data/CanadianFBP/400cellsC1 --output-instance-folder resutls/400cellsC1 --nsims 5 --out-messages --weather rows --sim C
+./Cell2Fire.Linux.x86_64 --input-instance-folder ../test/SB_Instance --output-folder ../test/test_results/SB --sim-years 1 --nsims 100 --output-messages --out-ros --out-fl --out-intensity --grids --sim S
