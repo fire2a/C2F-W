44bd58fcedd510575bc34dd2550562b1411c87e5
mati <matilde.rivas@ug.uchile.cl>
2024-12-09 12:20:04 -0300

    Use volumes to read input data and write output to host file system.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,10 +1,7 @@
 set -e
 
 function main() {
-    if [[ $# -lt 2 ]]; then
-        printf "\nMissing parameters\n"
-    else Cell2Fire/Cell2Fire $@
-    fi
+    Cell2Fire/Cell2Fire $@
 }
 
 main "$@"
