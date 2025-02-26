6027d169fefb91f9ed7b7d5df80ad3c3d86251dd
mati <matilde.rivas@ug.uchile.cl>
2025-02-26 17:26:27 -0300

    [fix] Try LDFLAGS

--- test/unit_tests/test_kitral.cpp
+++ test/unit_tests/test_kitral.cpp
@@ -2,19 +2,17 @@
 // Created by mati on 20-02-25.
 //
 #include "../../Cell2Fire/FuelModelKitral.h"
-#include <catch2/catch_all.hpp>
-// #include <catch2/matchers/catch_matchers_floating_point.hpp>
-using Catch::Matchers::WithinAbs;
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
 
 TEST_CASE(
-
 
     "Slope effect works correctly",
     "[slope_effect]"
     )
 {
-    REQUIRE_THAT(slope_effect(156.75, 170, 100), WithinAbs(1.0124, 0.001));
-    REQUIRE_THAT(slope_effect(201.124, 192.543, 100), WithinAbs(0.992, 0.001));
-    REQUIRE_THAT(slope_effect(201.124, 201.124, 100), WithinAbs(1, 0.001));
+    REQUIRE_THAT(slope_effect(156.75, 170, 100), Catch::Matchers::WithinAbs(1.0124, 0.001));
+    REQUIRE_THAT(slope_effect(201.124, 192.543, 100), Catch::Matchers::WithinAbs(0.992, 0.001));
+    REQUIRE_THAT(slope_effect(201.124, 201.124, 100), Catch::Matchers::WithinAbs(1, 0.001));
     // REQUIRE_THROWS(slope_effect(156.75, 170, 0));
 }
