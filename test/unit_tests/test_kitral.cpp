df5f49364e79d857d596390d917dac5557714000
mati <matilde.rivas@ug.uchile.cl>
2025-03-03 15:11:25 -0300

    [fix] add catch2 main definition

--- test/unit_tests/test_kitral.cpp
+++ test/unit_tests/test_kitral.cpp
@@ -1,18 +1,16 @@
 //
 // Created by mati on 20-02-25.
 //
+#define CATCH_CONFIG_MAIN
 #include "../../Cell2Fire/FuelModelKitral.h"
 #include <catch2/catch.hpp>
 
 using Catch::Matchers::WithinAbs;
 
-TEST_CASE(
-    "Slope effect works correctly",
-    "[slope_effect]"
-    )
+TEST_CASE("Slope effect works correctly", "[slope_effect]")
 {
-    REQUIRE_THAT(slope_effect(156.75, 170, 100), Catch::Matchers::WithinAbs(1.0124, 0.001));
-    REQUIRE_THAT(slope_effect(201.124, 192.543, 100), Catch::Matchers::WithinAbs(0.992, 0.001));
-    REQUIRE_THAT(slope_effect(201.124, 201.124, 100), Catch::Matchers::WithinAbs(1, 0.001));
+    REQUIRE_THAT(slope_effect(156.75, 170, 100), WithinAbs(1.0124, 0.001));
+    REQUIRE_THAT(slope_effect(201.124, 192.543, 100), WithinAbs(0.992, 0.001));
+    REQUIRE_THAT(slope_effect(201.124, 201.124, 100), WithinAbs(1, 0.001));
     // REQUIRE_THROWS(slope_effect(156.75, 170, 0));
 }
