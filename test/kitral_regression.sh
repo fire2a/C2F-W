ea9413966bd168ee88e474a015476bde6164aa5a
mati <matilde.rivas@ug.uchile.cl>
2025-05-05 12:51:11 -0400

    Generalize tests so it runs multiple instances

--- test/kitral_regression.sh
+++ test/kitral_regression.sh
@@ -1,80 +1,71 @@
 #!/bin/bash
-# run simulations from model, put them in test_results, compare to target_results
+# generalized regression test script for multiple Cell2Fire runs
 
-
-#rm -rf kitral_target_results
+# unzip target results
 unzip -q kitral_target_results.zip
 
-# recreate targets with tests
-# rm -rf target_results.zip
-# execute the for loop
-# mv test_results target_results
-# zip -r target_results.zip target_results
-# git add target_results.zip
-# git commit -m "Update target results"
-# git push
-
-# add to path
+# add Cell2Fire to PATH
 PATH=../Cell2Fire:$PATH
 
-# run
-set -x # enable debug tracing
-echo running Portillo
-output_folder=kitral_test_results/portillo
-mkdir -p $output_folder
+# define runs: name|input_folder|extra_args
+runs=(
+    "portillo|../data/Kitral/Portillo-asc/|--nsims 1 --seed 24 --fmc 50 --ignitions"
+    "villarrica|../data/Kitral/Villarrica-tif/|--nsims 10 --seed 42 --fmc 66 --FirebreakCells ../data/Kitral/Villarrica-tif/harvested_Random.csv"
+    # add more runs here following the same pattern
+)
 
-Cell2Fire$1 --input-instance-folder ../data/Kitral/Portillo-asc/ --output-folder $output_folder --grids  --output-messages --ignitions --out-fl --out-intensity  --ignitionsLog  --out-  --sim K --nsims 50 --seed 24 --fmc 50 --weather rows > $output_folder/log.txt
+# loop over each run
+for run in "${runs[@]}"; do
+    IFS='|' read -r name input_folder extra_args <<< "$run"
+    echo "Running simulation: $name"
 
-set +x # disable debug tracing
+    output_folder=kitral_test_results/$name
+    mkdir -p "$output_folder"
 
-# find difference between directories and files
+    set -x # enable debug tracing
+    Cell2Fire$1 --input-instance-folder "$input_folder" --output-folder "$output_folder" --grids --output-messages --out-fl --out-intensity --ignitionsLog --sim K --weather rows $extra_args > "$output_folder/log.txt"
+    set +x # disable debug tracing
 
-# define the directories to compare
-target="kitral_target_results"
+    # compare number of files
+    target="kitral_target_results/$name"
+    target_files=$(find "$target" -type f | sort)
+    target_num_files=$(echo "$target_files" | wc -l)
+    output_files=$(find "$output_folder" -type f | sort)
+    output_num_files=$(echo "$output_files" | wc -l)
 
-# get the list of files in each directory
-target_files=$(find ${target} -type f | sort)
-target_num_files=$(ls -1 $target_files | wc -l)
-# echo $target_files $target_num_files
-output_folder_files=$(find ${output_folder} -type f | sort)
-output_folder_num_files=$(ls -1 $output_folder_files | wc -l)
-# echo $output_folder_files $output_folder_num_files
+    if [ "$target_num_files" -ne "$output_num_files" ]; then
+        echo "Mismatch in file count for $name"
+        echo "Target: $target_num_files files"
+        echo "Output: $output_num_files files"
+        exit 1
+    fi
 
-# check if the number of files in each directory is equal
-if [ $target_num_files -ne $output_folder_num_files ]; then
-    echo "Directories are not equal due to number"
-    echo "Directory ${target} has ${target_num_files} files"
-    echo "Directory ${output_folder} has ${output_folder_num_files} files"
-    exit 1
-fi
-# delete version line
-find $output_folder/ -name log.txt -type f | xargs sed -i -e '/version:/d'
+    # clean version lines from logs
+    find "$output_folder" -name log.txt -type f | xargs sed -i -e '/version:/d'
 
-# use diff to compare the files in each directory
-diff_output=$(diff -rq "$target" "$output_folder")
-# echo $diff_output
+    # diff directories
+    diff_output=$(diff -rq "$target" "$output_folder")
+    if [ -z "$diff_output" ]; then
+        echo "$name: Directories are equal"
+    else
+        echo "$name: Differences found"
+        # detailed per-file check
+        for file1 in $(find "$target" -type f); do
+            file2=$(echo "$file1" | sed "s|$target|$output_folder|")
+            diff_result=$(diff "$file1" "$file2")
+            if [ -n "$diff_result" ]; then
+                echo "Difference in file: $file1"
+                echo "$diff_result"
+                exit 1
+            fi
+        done
+    fi
 
-# check if there is any difference
-if [ -z "$diff_output" ]; then
-    echo "Directories are equal"
-else
-    echo "Directories are not equal due to differences"
-    # compare file by file if fails show the file name
-    for file1 in $(find "$target" -type f); do
-        # find file1 in output_folder
-        file2=$(echo $file1 | sed "s/${target}/${output_folder}/")
-        # echo "Comparing $file1 and $file2"
-        diff_output=$(diff "$file1" "$file2")
-        if [ -n "$diff_output" ]; then
-            echo "Files are not equal, aborting... $file1"
-            echo $diff_output
-            exit 1
-        fi
-    done
-    #uncomment exit below to check all differences
-    #exit 1
-fi
+    # optionally clean up after each run
+    rm -r "$output_folder"
+done
 
+# clean up target results
 rm -r kitral_target_results
-rm -r $output_folder
+
 exit 0
