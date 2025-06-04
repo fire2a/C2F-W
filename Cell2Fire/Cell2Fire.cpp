73a078760e94dee5b1250a24ccfd2a1508e2c648
fdobad <fernandobadilla@hotmail.com>
2025-06-03 23:17:12 -0400

    simulation start formatting + debug print fire periods

--- Cell2Fire/Cell2Fire.cpp
+++ Cell2Fire/Cell2Fire.cpp
@@ -2117,9 +2117,11 @@ Cell2Fire::Step(boost::random::mt19937 generator, int ep)
             }
             else
             {
-                cout << "\nSimulation " << this->sim << " Start:\n\tweather file: " << WeatherHistory[this->sim]
+                cout << "\nSimulation " << this->sim << " Start:"
+                     << "\n\tweather file: " << WeatherHistory[this->sim]
                      << "\n\tweather periods: " << this->WeatherData.size() - 1
-                     << "\n\tignition cell: " << IgnitionHistory[this->sim] << endl;
+                     << "\n\tignition cell: " << IgnitionHistory[this->sim]
+                     << "\n\ttotal fire periods: " << this->totalFirePeriods << endl;
                 // Start sending messages
                 std::unordered_map<int, std::vector<int>> SendMessageList = this->SendMessages();
                 // Get Message
